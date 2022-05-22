7a6512582fc9359163b83827935f2d54558c0710
emildzy <emildzy3@yandex.ru>
2022-05-22 16:54:20 +0300

    first commit

---
+++ crypto_help_bot.py
@@ -0,0 +1,56 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from config import token
+from aiogram.dispatcher.filters import Text
+from aiogram.utils.markdown import hbold, hlink, hitalic, hunderline
+from services.crypto_parse import get_crypto_course
+from services.rss_parse import get_list_news
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=token, parse_mode=types.ParseMode.HTML)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome_message(message: types.Message):
+    start_buttons = ["Последние новости 📑", "Топ 3 криптовалюты 💸"]
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*start_buttons)
+
+    with open('start_message.txt', 'r') as file:
+        start_message = file.read()
+
+    await message.answer(text=start_message, reply_markup=keyboard)
+
+
+@dp.message_handler(Text(equals="Последние новости 📑"))
+async def get_news(message: types.Message):
+    await message.answer(text='Собираю новости для вас...')
+    news_dict = get_list_news()
+    for name_source in news_dict:
+        for news in news_dict[name_source]:
+            news_data = \
+                f'Источник: {name_source} \n' \
+                f'{hitalic(f"Дата публикации:")} {news["publication_time"]} \n' \
+                f'{hbold(f"Заголовок:")} {news["title"]} \n ' \
+                f'{hunderline("Содержание:")}' \
+                f'{news["description"]} \n' \
+                f'{hlink("Читать далее", news["link"])}'
+            await message.answer(news_data)
+
+
+@dp.message_handler(Text(equals="Топ 3 криптовалюты 💸"))
+async def get_crypto(message: types.Message):
+    await message.answer(text='Собираю данные о валюте для вас...')
+    crypto_dict = get_crypto_course()
+
+    for coin in crypto_dict:
+        result = \
+            f'{hbold(coin["name"])} >>> {coin["price"]}$ \n'  \
+            f'Изменение за 24 часа : {coin["changes_24_hours"]}'
+        await message.answer(result)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
---
+++ services/crypto_parse.py
@@ -0,0 +1,49 @@
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
+
+def get_html_page(url):
+    ua = UserAgent().random
+    headers = {
+        'User-Agent': ua,
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,'
+                  'application/signed-exchange;v=b3;q=0.9',
+    }
+
+    response = requests.get(url, headers=headers, timeout=5)
+
+    if response.ok:
+        return response.text
+    print(f'Ошибка - {response.status_code}')
+
+
+def get_data(data):
+    soup = BeautifulSoup(data, 'lxml')
+    trs = soup.find('table').find('tbody').find_all('tr')
+    crypto_dict = []
+    for tr in trs[:3]:
+        tds = tr.find_all('td')
+
+        name = tds[2].find('p').text
+        price = tds[3].find('a').text.split('$')[-1]
+
+        changes_24_hours_percent = tds[4].find('span').text
+        changes_24_hours_up_or_down = tds[4].find('span').span['class'][0].split('-')[-1]
+        changes_24_hours = f'{changes_24_hours_up_or_down} {changes_24_hours_percent}'
+
+        crypto_dict.append({
+            'name': name,
+            'price': price,
+            'changes_24_hours': changes_24_hours
+        })
+    return crypto_dict
+
+
+def get_crypto_course():
+    url = 'https://coinmarketcap.com/'
+    return get_data(get_html_page(url))
+
+
+if __name__ == '__main__':
+    get_crypto_course()
